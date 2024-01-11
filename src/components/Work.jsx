c416df9b6604bd5b89d7e3c8b547eb8fb69cc00a
BounceSensei <robinson.domingo11@gmail.com>
2024-01-12 02:30:11 +0800

    Edit Work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,34 +3,34 @@ import WorkItem from './WorkItems.jsx'
 
 const data =[
         {
-          "year": 2022,
-          "title": "The Adventure Begins",
-          "duration": "2 hours 30 minutes",
-          "details": "An epic journey of discovery and self-realization."
+          "year": 2019,
+          "title": "Received my Diploma",
+          "duration": "4 years",
+          "details": "I successfully completed my college education."
         },
         {
           "year": 2019,
-          "title": "Mysteries of the Cosmos",
-          "duration": "1 hour 45 minutes",
-          "details": "Exploring the wonders and enigmas of the universe."
-        },
-        {
-          "year": 2021,
-          "title": "Infinite Horizons",
-          "duration": "2 hours 10 minutes",
-          "details": "A cinematic exploration of boundless possibilities and dreams."
-        },
-        {
-          "year": 2023,
-          "title": "Tech Revolution: A Documentary",
-          "duration": "1 hour 55 minutes",
-          "details": "Unveiling the technological advancements shaping our future."
+          "title": "Technical Support within the HR division.",
+          "duration": "6 Months",
+          "details": "I recently graduated and have now secured my first position, specializing in Technical Support"
         },
         {
           "year": 2020,
-          "title": "Wildlife Chronicles",
-          "duration": "2 hours",
-          "details": "A breathtaking journey into the heart of the world's most diverse ecosystems."
+          "title": "Technical Support within the Finance division.",
+          "duration": "6 months",
+          "details": "Transfer in the finance division, providing technical support with a primary focus on data consolidation using Python."
+        },
+        {
+          "year": 2020,
+          "title": "Technical Support within the ICT division.",
+          "duration": "2 years",
+          "details": " involves providing assistance, troubleshooting, and solutions for various technical issues related to information and communication systems."
+        },
+        {
+          "year": 2023,
+          "title": "System data analyst within the ICT division.",
+          "duration": "1 year",
+          "details": "Analyzing and managing data to support organizational decision-making and improve system efficiency."
         }
       
 ]
@@ -38,7 +38,7 @@ const data =[
 const Work = () => {
     return (
         <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-            <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+            <h1 className='text-4xl font-bold text-center text-[#001b5e]'>School and Work Experience</h1>
             {data.map((item, idx)=>
                 <WorkItem 
                 key={idx}
